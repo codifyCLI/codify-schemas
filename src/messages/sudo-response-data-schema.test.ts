1370630008dddc03f737307b9f879277efb2addc
kevinwang <kevinwang5658@gmail.com>
2024-05-30 08:13:16 -0400

    Updated domain to codifycli. Added sudo request schema

---
+++ src/messages/sudo-response-data-schema.test.ts
@@ -0,0 +1,21 @@
+import schema from './sudo-request-data-schema.json';
+import {describe, expect, it} from 'vitest'
+import Ajv2020 from 'ajv/dist/2020.js'
+
+const ajv = new Ajv2020.default({
+  strict: true,
+})
+
+describe('Get resources response data schema', () => {
+  it('compiles', () => {
+    ajv.compile(schema);
+  })
+
+  it("requires an empty object", () => {
+    const validate = ajv.compile(schema);
+    expect(validate({
+      command: 'abc def'
+    })).to.be.true;
+  })
+
+})
