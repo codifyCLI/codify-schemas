49cfe405bb5044105758af86d776580a8ca782c2
kevinwang <kevinwang5658@gmail.com>
2025-01-16 10:08:11 -0500

    fix: Reverted back to static imports for JSON

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-const ConfigFileSchema = (await import('./config-file-schema.json', {assert: {type: 'json'}})).default
-const ProjectSchema = (await import('./project-schema.json', {assert: {type: 'json'}})).default
-const ResourceSchema = (await import('./resource-schema.json', {assert: {type: 'json'}})).default
-const IpcMessageSchema = (await import('./ipc-message-schema.json', {assert: {type: 'json'}})).default
-const IpcMessageV2Schema = (await import('./ipc-message-schema-v2.json', {assert: {type: 'json'}})).default
-const ApplyRequestDataSchema = (await import('./messages/apply-request-data-schema.json', {assert: {type: 'json'}})).default
-const ApplyResponseDataSchema = (await import('./messages/apply-response-data-schema.json', {assert: {type: 'json'}})).default
-const ErrorResponseDataSchema = (await import('./messages/error-response-data-schema.json', {assert: {type: 'json'}})).default
-const GetResourceInfoRequestDataSchema = (await import('./messages/get-resource-info-request-data-schema.json', {assert: {type: 'json'}})).default
-const GetResourceInfoResponseDataSchema = (await import('./messages/get-resource-info-response-data-schema.json', {assert: {type: 'json'}})).default
-const ImportRequestDataSchema = (await import('./messages/import-request-data-schema.json', {assert: {type: 'json'}})).default
-const ImportResponseDataSchema = (await import('./messages/import-response-data-schema.json', {assert: {type: 'json'}})).default
-const PlanRequestDataSchema = (await import('./messages/plan-request-data-schema.json', {assert: {type: 'json'}})).default
-const PlanResponseDataSchema = (await import('./messages/plan-response-data-schema.json', {assert: {type: 'json'}})).default
-const ValidateRequestDataSchema = (await import('./messages/validate-request-data-schema.json', {assert: {type: 'json'}})).default
-const ValidateResponseDataSchema = (await import('./messages/validate-response-data-schema.json', {assert: {type: 'json'}})).default
-const InitializeRequestDataSchema = (await import('./messages/initialize-request-data-schema.json', {assert: {type: 'json'}})).default
-const InitializeResponseDataSchema = (await import('./messages/initialize-response-data-schema.json', {assert: {type: 'json'}})).default
-const SudoRequestDataSchema = (await import('./messages/sudo-request-data-schema.json', {assert: {type: 'json'}})).default
-const SudoRequestResponseDataSchema = (await import('./messages/sudo-response-data-schema.json', {assert: {type: 'json'}})).default
+import ConfigFileSchema from './config-file-schema.json' with {type: 'json'}
+import ProjectSchema from './project-schema.json' with {type: 'json'}
+import ResourceSchema from './resource-schema.json' with {type: 'json'}
+import IpcMessageSchema from './ipc-message-schema.json' with {type: 'json'}
+import IpcMessageV2Schema from './ipc-message-schema-v2.json' with {type: 'json'}
+import ApplyRequestDataSchema from './messages/apply-request-data-schema.json' with {type: 'json'}
+import ApplyResponseDataSchema from './messages/apply-response-data-schema.json' with {type: 'json'}
+import ErrorResponseDataSchema from './messages/error-response-data-schema.json' with {type: 'json'}
+import GetResourceInfoRequestDataSchema from './messages/get-resource-info-request-data-schema.json' with {type: 'json'}
+import GetResourceInfoResponseDataSchema from './messages/get-resource-info-response-data-schema.json' with {type: 'json'}
+import ImportRequestDataSchema from './messages/import-request-data-schema.json' with {type: 'json'}
+import ImportResponseDataSchema from './messages/import-response-data-schema.json' with {type: 'json'}
+import PlanRequestDataSchema from './messages/plan-request-data-schema.json' with {type: 'json'}
+import PlanResponseDataSchema from './messages/plan-response-data-schema.json' with {type: 'json'}
+import ValidateRequestDataSchema from './messages/validate-request-data-schema.json' with {type: 'json'}
+import ValidateResponseDataSchema from './messages/validate-response-data-schema.json' with {type: 'json'}
+import InitializeRequestDataSchema from './messages/initialize-request-data-schema.json' with {type: 'json'}
+import InitializeResponseDataSchema from './messages/initialize-response-data-schema.json' with {type: 'json'}
+import SudoRequestDataSchema from './messages/sudo-request-data-schema.json' with {type: 'json'};
+import SudoRequestResponseDataSchema from './messages/sudo-response-data-schema.json' with {type: 'json'};
 
 export {
   ConfigFileSchema,
